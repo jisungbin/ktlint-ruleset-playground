f4bb14d9aacbd2108ed57252e21daf4f3bd4e8ab
Ji Sungbin <ji@sungb.in>
2025-06-13 23:54:49 +0900

    Update dependencies

--- ruleset/build.gradle.kts
+++ ruleset/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
   kotlin("jvm")
-  id("com.vanniktech.maven.publish") version "0.30.0"
+  id("com.vanniktech.maven.publish") version "0.32.0"
 }
 
 tasks.withType<Test> {
@@ -8,15 +8,15 @@ tasks.withType<Test> {
 }
 
 kotlin {
-  jvmToolchain(19)
+  jvmToolchain(21)
 }
 
 dependencies {
-  implementation("com.pinterest.ktlint:ktlint-cli-ruleset-core:1.5.0")
-  implementation("com.pinterest.ktlint:ktlint-rule-engine-core:1.5.0")
+  implementation("com.pinterest.ktlint:ktlint-cli-ruleset-core:1.6.0")
+  implementation("com.pinterest.ktlint:ktlint-rule-engine-core:1.6.0")
 
   testImplementation(kotlin("reflect"))
   testImplementation(kotlin("test-junit5"))
-  testImplementation("com.pinterest.ktlint:ktlint-test:1.5.0")
-  testRuntimeOnly("org.slf4j:slf4j-nop:2.0.16")
+  testImplementation("com.pinterest.ktlint:ktlint-test:1.6.0")
+  testRuntimeOnly("org.slf4j:slf4j-nop:2.0.17")
 }
